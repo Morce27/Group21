1f9ae4bff840ec96ddcd265d641bbad89115f80f
Morce27 <fufunanmarcgab@gmail.com>
2025-06-28 07:11:24 +0800

    Delete group21 directory

--- group21/dbConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseHost = 'localhost';
-$databaseName = 'nihao';
-$databaseUsername = 'root';
-$databasePassword = '';
-
-// Open a new connection to the MySQL server
-$conn = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
